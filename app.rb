506281ce3b57e8c810da7f3fce35bb5602af6998
Gray Gilmore <graygilmore@gmail.com>
2016-04-10 23:06:10 -0700

    Add three different taxonomy/taxon endpoints

--- app.rb
+++ app.rb
@@ -13,6 +13,9 @@ module Obversa
       namespace "/api" do
         get("/products.json") { json(send_file("mocks/products.json")) }
         get("/products/:id.json") { json(send_file("mocks/products/1.json")) }
+        get("/taxonomies/:id.json") { json(send_file("mocks/taxonomies/1.json")) }
+        get("/taxonomies/:taxonomy_id/taxons/:taxon_id.json") { json(send_file("mocks/taxons/1.json")) }
+        get("/taxons/products.json") { json(send_file("mocks/taxons/products.json")) }
       end
     end
   end
